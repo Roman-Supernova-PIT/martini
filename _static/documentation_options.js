d2eb05643616cc7d9f44935d7e013c541e89d157
benjaminrose <benjaminrose@users.noreply.github.com>
2026-07-16 16:43:02 +0000

    Delete unneeded template files (#3) 76792a3a692ad3459b2f3d038b41e74cc1232ca3

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+g2cb1dc959',
+    VERSION: '0.1.dev1+g76792a3a6.d20260716',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
